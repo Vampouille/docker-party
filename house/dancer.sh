d35001f4831ad38cc5143212b3ce4b696fc56cc4
Julien Acroute <julien.acroute@camptocamp.com>
2022-03-18 17:48:48 +0100

    Improve completion 💃

--- house/dancer.sh
+++ house/dancer.sh
@@ -1,50 +1,64 @@
 #!/bin/bash
 
-if [ "$1" != "list" ] && [ "$1" != "use" ] && [ "$1" != "completion" ]; then
-  echo "Invalid or no command : $1"
-  echo "Usage $0 use|list"
+if [ $# -gt 1 ]; then
+  echo "Invalid usage"
+  echo "Usage:"
+  echo "  * List dancer with: $0"
+  echo "  * Select dancer with: $0 <dancer UUID>"
   exit 1
 fi
 
-if [ $1 = "list" ]; then
-  for dancer_desc_file in $(ls /var/run/*.txt); do
-    dancer_name=$(cat ${dancer_desc_file})
-    dancer_uuid=$(basename ${dancer_desc_file} .txt)
-    echo "${dancer_uuid} ${dancer_name}"
-  done
+if [ -L "/var/run/docker.sock" ]; then
+  current_dancer_socket=$(readlink -f /var/run/docker.sock)
+  current_dancer_uuid=$(basename ${current_dancer_socket} .sock)
+  current_dancer_name=$(cat /var/run/${current_dancer_uuid}.txt)
+fi
+
+if [ $# -eq 0 ]; then
+  dancers_files=$(ls /var/run/*.txt 2> /dev/null || true)
+  if [ -n "${dancers_files}" ]; then
+    for dancer_desc_file in $(ls /var/run/*.txt 2> /dev/null || true); do
+      dancer_name=$(cat ${dancer_desc_file})
+      dancer_uuid=$(basename ${dancer_desc_file} .txt)
+      if [ $dancer_uuid = "$current_dancer_uuid" ]; then
+        echo -n "$(tput bold)"
+      fi
+      echo -n "${dancer_uuid} ${dancer_name}"
+      if [ $dancer_uuid = "$current_dancer_uuid" ]; then
+        echo " *💃*$(tput sgr0)"
+      else
+        echo ""
+      fi
+    done
+  else
+    echo "🏠 The dancerfloor is empty 🧶"
+  fi
+
 elif [ $1 = "completion" ]; then
+
   echo -n "("
-  #echo "(ITEM1:'DESC1' ITEM2:'DESC2')"
-  #exit 0
-  for dancer_desc_file in $(ls /var/run/*.txt); do
+  for dancer_desc_file in $(ls /var/run/*.txt 2> /dev/null || true); do
     dancer_name=$(cat ${dancer_desc_file})
     dancer_uuid=$(basename ${dancer_desc_file} .txt)
-    echo -n "${dancer_uuid}:'${dancer_name}' "
+    echo -n "${dancer_uuid}:'${dancer_name}"
+    if [ $dancer_uuid = "$current_dancer_uuid" ]; then
+      echo -n " *💃*"
+    fi
+    echo -n "' "
   done
   echo ")"
 
-elif [ $1 = "use" ]; then
-  dancer_uuid=$2
-  if [ -z "$2" ]; then
-    echo "No dancer specified"
-    echo "Usage: $0 use <dancer UUID>"
-    echo "See '$0 list' for a list of dancers"
-    exit 2
-  fi
-  if [ ! -S "/var/run/$2.sock" ]; then
-    echo "No such dancer: $2"
-    echo "No docker socket found at '/var/run/$2.sock'"
+else
+
+  dancer_uuid=$1
+  if [ ! -S "/var/run/${dancer_uuid}.sock" ]; then
+    echo "No such dancer: ${dancer_uuid}"
+    echo "No docker socket found at '/var/run/${dancer_uuid}.sock'"
     exit 3
   fi
   if [ -L /var/run/docker.sock ]; then
     rm /var/run/docker.sock
   fi
-  ln -s /var/run/$2.sock /var/run/docker.sock
-elif [ $1 = "get" ]; then
-  dancer_socket=$(readlink -f /var/run/docker.sock)
-  dancer_uuid=$(basename ${dancer_socket} .sock)
-  dancer_name=$(cat /var/run/${dancer_uuid}.txt)
-else
-  echo "Invalid command"
-  exit 4
+  ln -s /var/run/${dancer_uuid}.sock /var/run/docker.sock
+
 fi
