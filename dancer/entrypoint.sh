2cebed8a24a00750fa57c2a4f21d81ceb374ac3f
Julien Acroute <julien.acroute@camptocamp.com>
2023-07-25 13:39:31 +0200

    fix: remove interactive prompt

--- dancer/entrypoint.sh
+++ dancer/entrypoint.sh
@@ -6,6 +6,6 @@ name=$REPLY
 
 echo $name > /tmp/$uuid.txt
 
-scp -i /id_rsa /tmp/$uuid.txt $*:/var/run/
+scp -i /id_rsa -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' /tmp/$uuid.txt $*:/var/run/
 
 autossh -M 0 -N -o 'ServerAliveInterval 45' -o 'ServerAliveCountMax 2' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' -i /id_rsa -R /var/run/docker.sock:/var/run/$uuid.sock $*
