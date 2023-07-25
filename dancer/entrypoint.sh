f8dd66d1f98a51948e162e824a5d57729f550ad4
Julien Acroute <julien.acroute@camptocamp.com>
2023-07-25 15:52:57 +0200

    feat: use variable for port

--- dancer/entrypoint.sh
+++ dancer/entrypoint.sh
@@ -6,6 +6,6 @@ name=$REPLY
 
 echo $name > /tmp/$uuid.txt
 
-scp -i /id_rsa -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' /tmp/$uuid.txt $*:/var/run/
+scp -i /id_rsa -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' -P ${HOUSE_PORT} /tmp/$uuid.txt ${HOUSE_USER}@${HOUSE_HOST}:/var/run/
 
-autossh -M 0 -N -o 'ServerAliveInterval 45' -o 'ServerAliveCountMax 2' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' -i /id_rsa -R /var/run/docker.sock:/var/run/$uuid.sock $*
+autossh -M 0 -N -o 'ServerAliveInterval 45' -o 'ServerAliveCountMax 2' -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' si /id_rsa -R /var/run/docker.sock:/var/run/$uuid.sock -p ${HOUSE_PORT} ${HOUSE_USER}@${HOUSE_HOST}
